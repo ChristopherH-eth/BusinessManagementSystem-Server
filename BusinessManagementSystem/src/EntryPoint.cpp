8d05e281b7175876b48c38b27c97eecabfae36fa
Christopher Hardy <chris.hardy1989@hotmail.com>
2022-10-08 23:58:10 -0400

    add function processing based on client input

--- BusinessManagementSystem/src/EntryPoint.cpp
+++ BusinessManagementSystem/src/EntryPoint.cpp
@@ -15,7 +15,34 @@
  */
 void ListenerMessageReceived(TcpListener* listener, int client, std::string msg)
 {
-	std::cout << "\nClient message: " << msg << "\n" << std::endl;
+	std::cout << "\nClient message: " << msg << std::endl;
+
+	enum input
+	{
+		addEployee = 100,
+		removeEmployee = 101
+	};
+
+	std::string inputStr = msg.substr(0, 3);
+	int input = stoi(inputStr);
+
+	std::cout << "Function: " << input << "\n" << std::endl;
+
+	switch (input)
+	{
+	case 100:
+		std::cout << "Called addEmployee() function" << std::endl;
+		listener->Send(client, "addEmployee function called");
+		break;
+	case 101:
+		std::cout << "Called removeEmployee() function" << std::endl;
+		listener->Send(client, "removeEmployee function called");
+		break;
+	default:
+		std::cout << "No function called" << std::endl;
+		listener->Send(client, "No function called");
+		break;
+	}
 
 	listener->Send(client, msg);
 }
